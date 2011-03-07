4865765240b70710f941be55f998017f1c60eed4
TBeijen <tibobeijen@gmail.com>
2011-03-07 22:19:31 +0100

    Fine tuned behaviour: now, besides blur, only actually entering text will disable backspace behaviour

--- public/js/jquery.autofocusBackspace.js
+++ public/js/jquery.autofocusBackspace.js
@@ -4,6 +4,8 @@
  * jQuery plugin that focuses the first of the matching elements while
  * preserving backspace functionality if no text has yet been entered
  *
+ * TODO:
+ *  - If disabled, remove event handlers to prevent unneccessary code execution
  * Copyright (c) 2011 Tibo Beijen
  *
  * Dual licensed under the MIT and GPL licenses:
@@ -12,18 +14,29 @@
  */
 (function($){
     $.fn.autofocusBackspace = function() {
+        // declare vars & determine start values
         var _target = this.first();
+        var _initValue = _target.val();
         var _active = true;
         _target.focus();
+
+        // disable if input is actually changed
+        _target.keyup(function(event) {
+            if (_target.val() != _initValue) {
+                _active = false;
+            }
+        });
+
+        // disable if losing focus
+        _target.blur(function(event) {
+            _active = false;
+        });
+
+        // respond to backspace while active
         _target.keydown(function(event) {
             if (_active && event.keyCode == '8') {
                 history.back();
-            } else {
-                _active = false;
             }
-        _target.blur(function(event) {
-            _active = false;
-        })
         });
         return _target;
     };
